b502b17137dc26fe754bb686ee52de4b0005d238
SharpX72 <ekamsarao43@gmail.com>
2024-10-23 23:20:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7922405455:AAH3XWmmN4YSLvQDuJe1aRe_7NkNv5h-i18")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("SharpX72")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1009132250")
+
+
+
+
+
+
+
+  
